c0d997f64bce2b71bf25334ec95afd87b4a746f9
abdullah arafat <akhastalukder777@gmail.com>
2026-08-19 14:25:14 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1242,7 +1242,7 @@ export default function Home() {
             </div>
 
             <div>
-              Built for High Speed, SEO & Google AdSense Approval.
+              Built for High Speed.
             </div>
 
           </div>
